7d74c178ea7e9651d82f06d300f3e635a696abdf
Nikolai Berezovskii <nikolai@brzv.sk>
2025-02-01 01:16:16 +0400

    Implement Telegram Web App back button functionality in template component

--- app/template.tsx
+++ app/template.tsx
@@ -1,14 +1,32 @@
 'use client'
 
 import { useEffect } from 'react'
+import { useRouter, usePathname } from 'next/navigation'
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+  const pathname = usePathname()
+
   useEffect(() => {
     const script = document.createElement('script')
     script.src = 'https://telegram.org/js/telegram-web-app.js?56'
     script.async = false
     document.head.insertBefore(script, document.head.firstChild)
-  }, [])
+
+    script.onload = () => {
+      const webApp = window.Telegram?.WebApp
+      if (!webApp) return
+
+      webApp.ready()
+      
+      if (pathname !== '/') {
+        webApp.BackButton.show()
+        webApp.BackButton.onClick(() => router.push('/')) // TODO: Add a back button to previous page, not just home
+      } else {
+        webApp.BackButton.hide()
+      }
+    }
+  }, [pathname, router])
 
   return children
 } 
